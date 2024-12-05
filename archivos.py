d71d98e6445fe201903520424d4e3e473f180fcc
contaguia <kleguiar@gmail.com>
2024-12-05 12:52:54 -0500

    Add files via upload

---
+++ archivos.py
@@ -0,0 +1,10 @@
+import csv
+
+# Abrir el archivo CSV
+with open("poblacion.csv", "r") as archivo:
+    lector = csv.reader(archivo)
+    for fila in lector:
+        print(fila)  # Imprime cada fila como una lista
+            
+            
+
